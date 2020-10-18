d4f549697547debaa5b6aa3be8d0d90e0ff45968
mkhatami34 <koabarab361@gmail.com>
2020-10-18 21:04:39 +0700

    initial commit

---
+++ src/if1/pkg10119026/latihan13/relational/IF110119026Latihan13Relational.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119026.latihan13.relational;
+
+/**
+ *
+ * @author Asus
+ * NAMA     : Muhammad Khatami
+ * KELAS    : PBO
+ * NIM      : 10119026
+ * Deskripsi Program : Program ini berisi program untuk menampilkan relationalS
+ */
+public class IF110119026Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bill = 10;
+        int bi12 = 20;
+        System.out.println("a == b = " + (bill == bi12));
+        System.out.println("a != b = " + (bill != bi12));
+        System.out.println("a > b = " + (bill > bi12));
+        System.out.println("a < b = " + (bill < bi12));
+        System.out.println("b >= a = " + (bi12 >= bill));
+        System.out.println("b <= a = " + (bi12 <= bill));
+    }
+    
+}
